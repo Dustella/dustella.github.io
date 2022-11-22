2ba12897366c7fa2c0b04a194b789ebf9be153f8
Dustella <fdnoaivj@outlook.com>
2022-11-22 11:12:09 +0800

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import vue from '@astrojs/vue';
-import Unocss from 'unocss/astro'
-import UnocssVite from 'unocss/vite'
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import Unocss from "unocss/astro"
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(),vue({
-		plugins: [UnocssVite()]
-	}),Unocss()],
+  site: "https://dustella.net",
+  integrations: [mdx(), sitemap(), Unocss()],
 });
