e64deafb47e66ee2fcfb6ac3863258d51cf4756c
Dustella <fdnoaivj@outlook.com>
2023-01-14 11:50:38 +0800

    🐛 more styling fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import Unocss from "unocss/astro"
-
-import sitemap from "@astrojs/sitemap"
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import Unocss from 'unocss/astro'
+import presetMini from '@unocss/preset-mini'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dustella.net",
-  integrations: [mdx(), sitemap(), Unocss()],
-});
+  site: 'https://dustella.net',
+  integrations: [mdx(), sitemap(), Unocss({
+    theme: {
+      colors: {
+        bggray_l: '#cdcecf',
+        bgwhite: '##f5f5f5',
+
+      },
+    },
+    presets: [presetMini({ dark: 'media' })],
+  })],
+})
