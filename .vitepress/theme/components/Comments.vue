789f7d4b7ef67ae802ab5f82a6be59240156e8da
Dustella <fdnoaivj@outlook.com>
2023-03-30 02:01:29 +0800

    🐛 fix comment height issue

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -22,22 +22,35 @@ const { frontmatter, isDark } = useData()
     //   data-loading="lazy"
     //   crossorigin="anonymous"
     //   async -->
-    <Giscus
-      id="comments"
-      repo="dustella/dustella.github.io"
-      repo-id="R_kgDOHpBvMg"
-      category="General"
-      category-id="DIC_kwDOHpBvMs4CVYlQ"
-      mapping="pathname"
-      term="在这里评论"
-      reactions-enabled="1"
-      emit-metadata="0"
-      input-position="top"
-      :theme="isDark ? 'dark' : 'light'"
-      lang="en"
-      loading="lazy"
-    >
+    <ClientOnly>
+      <Giscus
+        id="comments"
+        repo="dustella/dustella.github.io"
+        repo-id="R_kgDOHpBvMg"
+        category="General"
+        category-id="DIC_kwDOHpBvMs4CVYlQ"
+        mapping="pathname"
+        term="在这里评论"
+        reactions-enabled="1"
+        emit-metadata="0"
+        input-position="top"
+        host="https://giscus.app"
+        :theme="isDark ? 'dark' : 'light'"
+        lang="en"
       />
-    </Giscus>
+    </ClientOnly>
   </div>
 </template>
+
+<!-- <style>
+#comments::part(iframe) {
+  max-width: 640px;
+  margin: auto;
+  display: flex;
+}
+
+.comments-container {
+  max-height: 640px;
+  overflow-y: scroll;
+}
+</style> -->
