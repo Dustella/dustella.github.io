7ea8349b6fa205240fdc346f95206af935fc8b21
Dustella <fdnoaivj@outlook.com>
2023-03-29 17:08:09 +0800

    🐛 fix comments

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -7,22 +7,35 @@ const { frontmatter } = useData()
 
 <template>
   <div v-if="!frontmatter.nocomment" class="comments-container">
+    <!-- //   id="comments"
+    //   repo="dustella/dustella.github.io"
+    //   repo-id="R_kgDOHpBvMg"
+    //   data-category="General"
+    //   data-category-id="DIC_kwDOHpBvMs4CVYlQ"
+    //   data-mapping="pathname"
+    //   data-strict="0"
+    //   data-reactions-enabled="1"
+    //   data-emit-metadata="0"
+    //   data-input-position="bottom"
+    //   data-theme="preferred_color_scheme"
+    //   data-lang="zh-CN"
+    //   data-loading="lazy"
+    //   crossorigin="anonymous"
+    //   async -->
     <Giscus
       id="comments"
       repo="dustella/dustella.github.io"
       repo-id="R_kgDOHpBvMg"
-      data-category="General"
-      data-category-id="DIC_kwDOHpBvMs4CVYlQ"
-      data-mapping="pathname"
-      data-strict="0"
-      data-reactions-enabled="1"
-      data-emit-metadata="0"
-      data-input-position="bottom"
-      data-theme="preferred_color_scheme"
-      data-lang="zh-CN"
-      data-loading="lazy"
-      crossorigin="anonymous"
-      async
+      category="General"
+      category-id="DIC_kwDOHpBvMs4CVYlQ"
+      mapping="pathname"
+      term="在这里评论"
+      reactions-enabled="1"
+      emit-metadata="0"
+      input-position="top"
+      theme="preferred_color_scheme"
+      lang="en"
+      loading="lazy"
     >
       />
     </Giscus>
