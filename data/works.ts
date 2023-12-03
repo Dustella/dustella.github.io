0e3168eee63361d4d245704e73c5b9a4de9e7ae2
Dustella <fdnoaivj@outlook.com>
2023-12-04 01:36:54 +0800

    ✨ fix works

--- data/works.ts
+++ data/works.ts
@@ -17,14 +17,14 @@ const works: MyWorks = {
 
       title: 'NUISTCraft',
       details: '一个 NUIST 的 Minecraft 服务器',
-      link: 'https://nuistcraft.com',
+      link: 'https://docs.nuistcraft.com',
       icon: { src: 'https://nuistcraft.com/img/undraw_docusaurus_mountain.svg', height: '15px' },
     },
     {
 
       title: 'NuistShare',
       details: '资料共享',
-      link: 'https://nuistshare.com',
+      link: 'https://www.nuistshare.cn',
       icon: { src: 'https://nuistcraft.com/img/undraw_docusaurus_mountain.svg', height: '15px' },
     },
   ],
